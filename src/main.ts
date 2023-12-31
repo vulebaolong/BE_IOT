9d45db012aaf0040db2326022e45d92cf92ae9ba
Vũ Lê Bảo Long <vulebaolong@gmail.com>
2023-12-31 22:45:03 +0700

    first commit

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, VersioningType } from '@nestjs/common';
+
+async function bootstrap() {
+    const logger = new Logger('Bootstrap');
+    const app = await NestFactory.create(AppModule);
+
+    app.enableCors({
+        // origin: '*', //=> Allow all access
+        origin: true, //=> allow same domain eg: localhost
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        preflightContinue: false,
+        credentials: true, // FE and BE are both open to exchange cookies
+    });
+
+    // Version APIs
+    app.setGlobalPrefix('api');
+    app.enableVersioning({
+        type: VersioningType.URI,
+        defaultVersion: ['1'],
+    });
+    
+    await app.listen(3000).then(() => {
+        logger.verbose(`App is running on http://localhost:${3000}`);
+    });
+}
+bootstrap();
