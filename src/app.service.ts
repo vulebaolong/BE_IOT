59729da28c5ea757005ef0c0b3b300f875500419
Vũ Lê Bảo Long <vulebaolong@gmail.com>
2023-12-31 23:08:02 +0700

    oke

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    console.log(123);
-    return 'Hello World!';
+    console.log(Date.now());
+    return `${Date.now()}`;
   }
 }
