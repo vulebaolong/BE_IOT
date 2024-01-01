618477be5f9156c67e0583d1386c4dc1b1db36aa
Vũ Lê Bảo Long <vulebaolong@gmail.com>
2024-01-01 14:33:04 +0700

    oke

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, VersioningType } from '@nestjs/common';
 import { useSwagger } from './common/swagger/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const logger = new Logger('Bootstrap');
@@ -24,6 +25,14 @@ async function bootstrap() {
 
     // swagger
     // useSwagger(app);
+    const config = new DocumentBuilder()
+        .setTitle('Cats example')
+        .setDescription('The cats API description')
+        .setVersion('1.0')
+        .addTag('cats')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('swagger', app, document);
 
     await app.listen(3000).then(() => {
         logger.verbose(`App is running on http://localhost:${3000}`);
