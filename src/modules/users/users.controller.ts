b50be70f9c2fcfe954ee2b115c3dec3a97cf826f
Vũ Lê Bảo Long <vulebaolong@gmail.com>
2024-01-01 15:23:07 +0700

    oke

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -2,7 +2,10 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { TAG_MODULE_USER } from '../../common/contants/swagger.contants';
 
+@ApiTags(TAG_MODULE_USER)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
